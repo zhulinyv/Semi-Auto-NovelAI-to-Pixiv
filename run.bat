f5d783563f43fbaa6e36916884e5ddcd3a28f9bc
zhulinyv <zhulinyv2005@outlook.com>
2024-04-02 22:00:52 +0800

    :children_crossing: 检查文件夹

--- run.bat
+++ run.bat
@@ -10,6 +10,9 @@ set PYTHON="venv\Scripts\Python.exe"
 : check pip packages
 %PYTHON% -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
+: check directories
+%PYTHON% .\utils\predir.py
+
 : have fun :)
 %PYTHON% .\main.py
 
