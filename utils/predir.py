ac2bab25d645761640d522dfa0359bbca925f9fc
zhulinyv <zhulinyv2005@outlook.com>
2024-04-11 10:39:37 +0800

    :bookmark: 发布新版

--- utils/predir.py
+++ utils/predir.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-need_dir_list = ["./output", "./output/t2i", "./output/choose_to_i2i", "./output/i2i/", "./output/pixiv", "./output/choose_to_upscale", "./output/upscale", "./output/mosaic", "./output/choose_to_mosaic", "./output/inpaint", "./output/inpaint/img", "./output/inpaint/mask", "./files/else_upscale_engine", "./files/prompt", "./files/prompt/done", "./output/choose_to_water", "./output/water"]
+need_dir_list = ["./output", "./output/t2i", "./output/choose_to_i2i", "./output/i2i/", "./output/pixiv", "./output/choose_to_upscale", "./output/upscale", "./output/mosaic", "./output/choose_to_mosaic", "./output/inpaint", "./output/inpaint/img", "./output/inpaint/mask", "./files/else_upscale_engine", "./files/prompt", "./files/prompt/done", "./output/choose_to_water", "./output/water", "./output/info_file", "./output/info_removed", "./output/info_reverted"]
 
 
 if not os.path.exists(".env"):
