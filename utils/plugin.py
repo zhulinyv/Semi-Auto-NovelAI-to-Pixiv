97488209846063b4b52a063358ad364ff290a528
zhulinyv <zhulinyv2005@outlook.com>
2024-04-30 18:16:55 +0800

    :zap: 优化插件导入代码

--- utils/plugin.py
+++ utils/plugin.py
@@ -4,28 +4,19 @@ import os
 
 def load_plugins(directory):
     plugins = {}
-    for root, dirs, files in os.walk(directory):
-        try:
-            for dir in dirs:
-                if dir == "__pycache__":
-                    pass
-                else:
-                    plugin_name = dir
-                    module_name = f"{directory}.{dir}"
-                    spec = importlib.util.spec_from_file_location(
-                        module_name, os.path.join(directory, dir, "__init__.py")
-                    )
-                    module = importlib.util.module_from_spec(spec)
-                    spec.loader.exec_module(module)
-                    plugins[plugin_name] = module
-            for file in files:
-                if file.endswith(".py") and file != "__init__.py":
-                    plugin_name = os.path.splitext(file)[0]
-                    module_name = f"{directory}.{plugin_name}"
-                    spec = importlib.util.spec_from_file_location(module_name, os.path.join(directory, file))
-                    module = importlib.util.module_from_spec(spec)
-                    spec.loader.exec_module(module)
-                    plugins[plugin_name] = module
-        except FileNotFoundError:
-            pass
+    plugin_list = os.listdir(directory)
+    for plugin in plugin_list:
+        if plugin.endswith(".py"):
+            location = os.path.join(directory, plugin)
+        elif plugin != "__pycache__":
+            location = os.path.join(directory, plugin, "__init__.py")
+        else:
+            location = None
+        if location:
+            plugin_name = plugin
+            module_name = f"{directory}.{plugin_name}"
+            spec = importlib.util.spec_from_file_location(module_name, location)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            plugins[plugin_name] = module
     return plugins
