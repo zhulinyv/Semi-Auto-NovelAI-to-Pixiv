a8d5446ee6daa87349682d8ef6e2b63d5684ace0
zhulinyv <zhulinyv2005@outlook.com>
2024-05-16 22:09:22 +0800

    :adhesive_bandage: 支持新增配置在界面更改

--- src/setting.py
+++ src/setting.py
@@ -4,12 +4,19 @@ def modify_env(**kwargs: dict):
         new_value = kwargs[target_key]
         with open(".env", "r", encoding="utf-8") as f:
             lines = f.readlines()
-        for i, line in enumerate(lines):
-            if line.startswith(target_key + "="):
-                lines[i] = f"{target_key}={new_value}\n"
-                break
-        with open(".env", "w", encoding="utf-8") as f:
-            f.writelines(lines)
+            f.seek(0)
+            setting = f.read()
+            print(setting)
+        if target_key not in setting:
+            with open(".env", "w", encoding="utf-8") as f:
+                f.write(setting + f"\n{target_key}={new_value}\n")
+        else:
+            for i, line in enumerate(lines):
+                if line.startswith(target_key + "="):
+                    lines[i] = f"{target_key}={new_value}\n"
+                    break
+            with open(".env", "w", encoding="utf-8") as f:
+                f.writelines(lines)
     return "修改已保存, 重启后生效!"
 
 
