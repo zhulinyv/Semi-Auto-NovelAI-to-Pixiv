5852ad3031d6178ba506756bb56ee70310076890
zhulinyv <zhulinyv2005@outlook.com>
2024-06-10 21:31:23 +0800

    :zap: 修改实例化位置以提升启动速度

--- src/tagger.py
+++ src/tagger.py
@@ -1,15 +1,8 @@
 from pathlib import Path
 
-from gradio_client import Client, file
 from loguru import logger
 
 from utils.utils import file_namel2pathl, file_path2list, file_path2name
-
-try:
-    client = Client("SmilingWolf/wd-tagger", verbose=False)
-except Exception:
-    pass
-
 
 # Dataset v3 series of models:
 SWINV2_MODEL_DSV3_REPO = "SmilingWolf/wd-swinv2-tagger-v3"
@@ -57,8 +50,14 @@ def tagger(
     character_thresh,
     character_mcut_enabled,
 ):
+    from gradio_client import Client, file
+
+    try:
+        client = Client("SmilingWolf/wd-tagger", verbose=False)
+    except Exception:
+        pass
+
     if batch:
-        # imgs_list = [Path(path) / i for i in file_path2list(path)]
         imgs_list = file_namel2pathl(file_path2list(path), Path(path))
     else:
         image.save("./output/temp.png")
@@ -71,12 +70,12 @@ def tagger(
                 try:
                     logger.info(f"正在反推: {img}...")
                     result = client.predict(
-                        file(img),  # filepath  in 'Input' Image component
-                        model_repo,  # Literal['SmilingWolf/wd-swinv2-tagger-v3', 'SmilingWolf/wd-convnext-tagger-v3', 'SmilingWolf/wd-vit-tagger-v3', 'SmilingWolf/wd-v1-4-moat-tagger-v2', 'SmilingWolf/wd-v1-4-swinv2-tagger-v2', 'SmilingWolf/wd-v1-4-convnext-tagger-v2', 'SmilingWolf/wd-v1-4-convnextv2-tagger-v2', 'SmilingWolf/wd-v1-4-vit-tagger-v2']  in 'Model' Dropdown component
-                        general_thresh,  # float (numeric value between 0 and 1) in 'General Tags Threshold' Slider component
-                        general_mcut_enabled,  # bool  in 'Use MCut threshold' Checkbox component
-                        character_thresh,  # float (numeric value between 0 and 1) in 'Character Tags Threshold' Slider component
-                        character_mcut_enabled,  # bool  in 'Use MCut threshold' Checkbox component
+                        file(img),
+                        model_repo,
+                        general_thresh,
+                        general_mcut_enabled,
+                        character_thresh,
+                        character_mcut_enabled,
                         api_name="/predict",
                     )
                     if batch:
