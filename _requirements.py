db09807cc00a3e4664569a8652de282777194c6a
zhulinyv <zhulinyv2005@outlook.com>
2025-02-10 14:45:05 +0800

    :goal_net: 添加注释以防止审查不通过

--- _requirements.py
+++ _requirements.py
@@ -2,10 +2,10 @@ import subprocess
 import sys
 
 try:
-    import main
-    from files.SANP_DOCS import launch
-    from src import *
-    from utils import *
+    import main  # noqa
+    from files.SANP_DOCS import launch  # noqa
+    from src import *  # noqa
+    from utils import *  # noqa
 except ModuleNotFoundError:
     print("检测到是首次启动, 开始安装所需依赖...")
     subprocess.call(f"{sys.executable} -s -m pip install -r requirements.txt")
